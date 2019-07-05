488bc2e24199b1c480d4523717a85ce66d7b831c
Paravjot Singh <41161923+singhparavjot@users.noreply.github.com>
2019-07-05 11:19:28 +0530

    Add files via upload

---
+++ speechTT.py
@@ -0,0 +1,17 @@
+import speech_recognition as sr
+
+r = sr.Recognizer()
+
+audio = 'ai.wav'
+
+with sr.AudioFile(audio) as source:
+    audio = r.record(source)
+    print ('Done!')
+
+try:
+    text = r.recognize_google(audio)
+    print (text)
+    
+except Exception as e:
+    print (e)
+
